3a96e1ab35adf2d3776357e32b368658dc1da889
antonio.gimenez <antonio.gimenez@DESKTOP-9MH23G4>
2022-10-25 13:03:08 +0200

    Ejercicios bucles

---
+++ src/tema3/pkg3_bucles/Ejercicio13.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tema3.pkg3_bucles;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author antonio.gimenez
+ */
+public class Ejercicio13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner teclado=new Scanner(System.in);
+        int num;   //***EXPLICAR PORQUE NO PUEDO DECLARARLO DENTRO DEL DO..WHILE
+        
+        //http://nosolomates.es/ayuda/ayuda/divisores.htm
+        
+        System.out.println("   CALCULO DE DIVISORES");
+        System.out.println("===========================");
+        do{
+        
+            System.out.print("Dime un numero (0 para salir):");
+            num=Integer.parseInt(teclado.nextLine());
+
+            for (int i = 1; i <= num; i++) {  //i son los posibles divisores
+                if (num%i==0){
+                    System.out.println("  El numero "+i+" es divisor de "+num);
+                }
+            }
+            System.out.println("");
+     
+        }while (num!=0);    
+        
+        //Sería muy fácil sacar si el numero num es primo, no?
+    }
+    
+}
